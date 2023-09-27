edb70cfacc4e3f4d5d70f3ded845f3c0b7424347
Thales Juliani <thales.juliani@gmail.com>
2023-09-27 15:57:55 -0400

    Teste 2

---
+++ Report Dashboard/reportDashboard.js
@@ -0,0 +1,19 @@
+console.log("Training 1 is working")
+
+let nameInsert = prompt("What the numer of your group?.....")
+
+const go =  document.querySelector.bind(document)
+
+function addName() {
+    go('#outputname').innerHTML = nameInsert
+    console.log(nameInsert)  
+}addName ()
+
+
+const image = new Image(100, 200)
+image.src = "sophiesump.jpeg";
+
+function addPhoto() {
+  go('#outputphoto').innerHTML = image
+   
+}addPhoto ()
